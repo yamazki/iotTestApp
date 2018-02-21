85c6bc439be8df794b2feb4b8abacadd1715bc77
yamazaki <ma17123@shibaura-it.ac.jp>
2018-02-21 21:28:23 +0900

    graph finished

--- node/app.js
+++ node/app.js
@@ -1,6 +1,10 @@
 const app = require("express")();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const ejs = require('ejs');
+
+// ejsを使用するための設定
+app.engine('ejs',ejs.renderFile);
 
 app.get("/data", function(req, res){
   console.log(req.query);
@@ -8,15 +12,19 @@ app.get("/data", function(req, res){
   res.send();
 });
 
+//ejs
+//app.get('/', function(req, res) {
+//    res.render('index.ejs', {title : 'graph'});
+//});
+
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/html/index.html");
 });
-
 
 io.on("connection", function(socket){
   console.log("a user connected");
 });
 
 http.listen(80, function(){
-  console.log("listening on *:3000");
+  console.log("listening on *:80");
 });
