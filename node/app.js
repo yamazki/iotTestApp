ab08726999ef4106068ea9b69505bfefccda1e23
yamazaki <ma17123@shibaura-it.ac.jp>
2018-02-21 15:35:12 +0900

    graph + websocket

--- node/app.js
+++ node/app.js
@@ -4,12 +4,14 @@ const io = require('socket.io')(http);
 
 app.get("/data", function(req, res){
   console.log(req.query);
+  io.emit("new lux", req.query);
   res.send();
 });
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/html/index.html");
 });
+
 
 io.on("connection", function(socket){
   console.log("a user connected");
