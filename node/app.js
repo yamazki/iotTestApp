fad0b1bbc1f86357d6c20af3312c7e682d5d4074
root <root@localhost.localdomain>
2018-03-09 20:28:55 +0900

    set mysql

--- node/app.js
+++ node/app.js
@@ -2,9 +2,29 @@ const app = require("express")();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const ejs = require('ejs');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host : 'mysql',
+  user : 'root',
+  password : 'password',
+  database: 'sensor',
+});
+
+connection.connect();
+
+//insert test
+//connection.query('INSERT INTO lux VALUES ("2000-01-01 10:00:00", 100)', function (error, results, fields) {
+//  console.log(results);
+//})
+//
+
+//select test
+connection.query('SELECT * FROM lux ', function (error, results, fields) {
+  console.log(results);
+})
 
 // ejsを使用するための設定
-app.engine('ejs',ejs.renderFile);
+//app.engine('ejs',ejs.renderFile);
 
 app.get("/data", function(req, res){
   console.log(req.query);
@@ -26,5 +46,5 @@ io.on("connection", function(socket){
 });
 
 http.listen(80, function(){
-  console.log("listening on *:80");
+  console.log("listening on port:80");
 });
